de3b566746bd3e5a0fe827301ed61738e176cbc8
Caique Alves <caiquewin@gmail.com>
2020-07-28 19:36:08 -0300

    Exercicio Prof

---
+++ Exercicio_EstruturaRepetitivaFOR_ProfNelio_PT06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_EstruturaRepetitivaFOR_ProfNelio_PT06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("coloque um numero que mostraremos todos divisores por ele !");
+            int valor = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= valor; i++)
+            {
+                if (valor % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
